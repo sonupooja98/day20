893d76bc87afc6e4390d9e30d682be636719f5ea
sonupooja <sonupooja2398@gmail.com>
2021-10-20 23:44:58 +0530

    Added:email address

--- src/com/bridglab/Junit/UserRegistration.java
+++ src/com/bridglab/Junit/UserRegistration.java
@@ -6,15 +6,39 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-public boolean firstName(String firstName) {
+	/*
+	 * method to check first name
+	 */
+	public boolean firstName(String firstName) {
+
+		String regex="^[A-z]{1}[a-z]*";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher match=pattern.matcher(firstName);
+
+		return match.matches();
+
+	}
+
+	/*
+	 * method to chek last name 
+	 */
 	
-	String regex="^[A-z]{1}[a-z]*";
-	Pattern pattern=Pattern.compile(regex);
-	Matcher match=pattern.matcher(firstName);
-	
-	return match.matches();
-	
-}
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{4}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
+	/*
+	 * method to check email address
+	 */
+	  public boolean emailAddress(String email) {
+	        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(email);
+	        return matcher.matches();
+	    }
+
 
 
 }
--- src/com/bridglab/Junit/UserRegistrationTest.java
+++ src/com/bridglab/Junit/UserRegistrationTest.java
@@ -6,20 +6,52 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
 
-    UserRegistration userRegistration = new UserRegistration();
+	UserRegistration userRegistration = new UserRegistration();
 
 
-    @Test
-    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Pooja");
-        Assert.assertEquals(true, result);
-    }
+	@Test
+	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Ashish");
+		Assert.assertEquals(true, result);
+	}
 
-    @Test
-    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-        boolean result = userRegistration.firstName("Poo$a");
-        Assert.assertEquals(false, result);
-    }
+	@Test
+	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("ashish");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Surja");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("surja");
+		Assert.assertEquals(false, result);
+	}
+	/*
+	 * methods to check mail addresss
+	 */
+	@Test
+	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.emailAddress("abc@gmail.com.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.emailAddress("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
+	}
+
+
+
+
 
 
 }
+
+
