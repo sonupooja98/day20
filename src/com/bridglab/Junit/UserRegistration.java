ed51025582ccbde502931f0fc82d0ef61c5b5a65
sonupooja <sonupooja2398@gmail.com>
2021-10-20 23:27:35 +0530

    welcome junit testing

---
+++ src/com/bridglab/Junit/UserRegistration.java
@@ -0,0 +1,20 @@
+package com.bridglab.Junit;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+public boolean firstName(String firstName) {
+	
+	String regex="^[A-z]{1}[a-z]*";
+	Pattern pattern=Pattern.compile(regex);
+	Matcher match=pattern.matcher(firstName);
+	
+	return match.matches();
+	
+}
+
+
+}
---
+++ src/com/bridglab/Junit/UserRegistrationTest.java
@@ -0,0 +1,25 @@
+package com.bridglab.Junit;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+    UserRegistration userRegistration = new UserRegistration();
+
+
+    @Test
+    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Pooja");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("Poo$a");
+        Assert.assertEquals(false, result);
+    }
+
+
+}
