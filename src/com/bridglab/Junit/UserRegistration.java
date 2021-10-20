02ff4e23a58bd7b72a7d14b05812444b78caa7ab
sonupooja <sonupooja2398@gmail.com>
2021-10-21 00:01:34 +0530

    Added:password rule 3

--- src/com/bridglab/Junit/UserRegistration.java
+++ src/com/bridglab/Junit/UserRegistration.java
@@ -1,4 +1,5 @@
 package com.bridglab.Junit;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -69,5 +70,15 @@ public class UserRegistration {
 
 	}
 
+	/*
+	 * Should have at least 1 numeric number in the password 
+	 */
+	public boolean passwordRule3(String password) {
+		String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9]).{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+
+	}
 
 }
